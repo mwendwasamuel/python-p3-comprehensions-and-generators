55e4777dfae5020c904f3e0ff9de6c0a45f30f6d
samuel <samuel.mbula@student.moringaschool.com>
2023-08-21 14:03:39 +0300

    completed python-p3-comprehension-and-generators-lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
-def return_evens(num_list):
-    pass
+def return_evens(sequence):
+    return [num for num in sequence if num % 2 == 0]
 
-def make_exclamation(sentence_list):
-    pass
+# Test the function
+print(return_evens([0, 1, 3, 5, 7, 8, 9]))
+
+
+def make_exclamation(sentences):
+    return [sentence + "!" for sentence in sentences]
+
+# Test the function
+print(make_exclamation(["Hello", "I'm doing great", "Python is fun"]))
+
